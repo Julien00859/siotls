5d8d918f4d69cab7d079520ea8adaf6bbf3c03a0
Julien Castiaux <julien.castiaux@gmail.com>
2023-08-15 16:05:25 +0200

    fix(extensions): signature algo can be in cert req

--- src/siotls/extensions/signature_algorithms.py
+++ src/siotls/extensions/signature_algorithms.py
@@ -5,4 +5,4 @@ from . import Extension
 
 class SignatureAlgorithms(Extension, SerializableBody):
     extension_type = ExtensionType.SIGNATURE_ALGORITHMS
-    _handshake_types = {HT.CLIENT_HELLO}
+    _handshake_types = {HT.CLIENT_HELLO, HT.CERTIFICATE_REQUEST}
