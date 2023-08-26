27af00307b6040b2a45b192b83263b85531585c1
Julien Castiaux <julien.castiaux@gmail.com>
2023-08-26 17:42:05 +0200

    imp(extensions): Cookie

--- src/siotls/extensions/cookie.py
+++ src/siotls/extensions/cookie.py
@@ -1,8 +1,32 @@
+import textwrap
 from siotls.iana import ExtensionType, HandshakeType as HT
-from siotls.serial import SerializableBody
+from siotls.serial import SerializableBody, SerialIO
 from . import Extension
 
 
 class Cookie(Extension, SerializableBody):
     extension_type = ExtensionType.COOKIE
     _handshake_types = {HT.CLIENT_HELLO}
+
+    _struct = textwrap.dedent("""\
+        struct {
+            opaque cookie<1..2^16-1>;
+        } Cookie;
+    """).strip()
+    cookie: bytes
+
+    def __init__(self, cookie):
+        self.cookie = cookie
+
+    @classmethod
+    def parse_body(cls, data):
+        stream = SerialIO(data)
+        cookie = stream.read_var(2)
+        stream.assert_eof()
+        return cls(cookie)
+
+    def serialize_body(self):
+        return b''.join([
+            len(self.cookie).to_bytes(2, 'big'),
+            self.cookie,
+        ])
