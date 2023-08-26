5547cbab2a8239d89d42cf75a4ab5c55cb75bca1
Julien Castiaux <julien.castiaux@gmail.com>
2023-08-26 14:03:08 +0200

    imp(extensions): padding

--- src/siotls/extensions/padding.py
+++ src/siotls/extensions/padding.py
@@ -1,3 +1,4 @@
+import textwrap
 from siotls.iana import ExtensionType, HandshakeType as HT
 from siotls.serial import SerializableBody
 from . import Extension
@@ -6,3 +7,20 @@ from . import Extension
 class Padding(Extension, SerializableBody):
     extension_type = ExtensionType.PADDING
     _handshake_types = {HT.CLIENT_HELLO}
+
+    _struct = textwrap.dedent("""
+        struct {
+            opaque zeros[Extension.extension_length];
+        } PaddingExtension;
+    """).strip()
+    zeros: bytes
+
+    def __init__(self, zeros):
+        self.zeros = zeros
+
+    @classmethod
+    def parse_body(cls, data):
+        return cls(data)
+
+    def serialize_body(self):
+        return self.zeros
