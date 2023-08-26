534ea23fcc53b4b5f41314f2c7a69bcbf5a4aec6
Julien Castiaux <julien.castiaux@gmail.com>
2023-08-26 18:11:15 +0200

    imp(extensions): PostHandshakeAuth

--- src/siotls/extensions/post_handshake_auth.py
+++ src/siotls/extensions/post_handshake_auth.py
@@ -1,8 +1,23 @@
 from siotls.iana import ExtensionType, HandshakeType as HT
-from siotls.serial import SerializableBody
+from siotls.serial import SerializableBody, TooMuchData
 from . import Extension
 
 
 class PostHandshakeAuth(Extension, SerializableBody):
     extension_type = ExtensionType.POST_HANDSHAKE_AUTH
     _handshake_types = {HT.CLIENT_HELLO}
+
+    _struct = ''
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def parse_body(cls, data):
+        if data:
+            msg = f"Expected end of stream but {len(data)} bytes remain."
+            raise TooMuchData(msg)
+        return cls()
+
+    def serialize_body(self):
+        return b''
