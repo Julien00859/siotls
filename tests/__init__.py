34435c30bb349a31e848606c844ba4ef566f74b2
Julien Castiaux <julien.castiaux@gmail.com>
2023-11-17 19:51:07 +0100

    imp: better verbosity scale in unittest

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,10 @@
-import sys
+import argparse
 import logging
 from siotls.__main__ import setup_logging
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', dest='verbosity', action='count', default=0)
+options, _ = parser.parse_known_args()
+
 logging.basicConfig()
-setup_logging(logging.DEBUG if '-vv' in sys.argv else logging.INFO)
+setup_logging(logging.WARNING - 10 * options.verbosity)
