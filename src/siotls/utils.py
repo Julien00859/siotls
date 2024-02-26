caa858f02da1c765de4bda20fe2e3f828bd3ea38
Julien Castiaux <julien.castiaux@gmail.com>
2024-02-26 16:20:09 +0100

    fix: magic numbers in hexdump
    I have been told that all the magic numbers inside the hexdump function
    were confusing people. We had an argument about the ascii table, it
    turns out that I was the only one in the room who knew the exact value
    of many ascii codepoints.
    
    As part of those silly (:p) comments, the whitespaces (\v, \t, \n, \r)
    are now represented using a whitespace (" ").

--- src/siotls/utils.py
+++ src/siotls/utils.py
@@ -9,10 +9,10 @@ def hexdump(bytes_):
     r"""
     Produce a pretty hexdump suitable for human reading.
 
-    >>> hexdump(b'\x00\x17Hello world!\nSweat day.\x00')
-    '''\
-    0000: 00 17 48 65 6c 6c 6f 20  77 6f 72 6c 64 21 0a 53  0.Hello  world!.S
-    0010: 77 65 61 74 20 64 61 79  2e 00                    weat day .0'''
+    >>> print(hexdump(b'\x00\x17Hello world!\nSweat day.\x00'))
+    0000: 00 17 48 65 6c 6c 6f 20  77 6f 72 6c 64 21 0a 53  0.Hello  world! S
+    0010: 77 65 61 74 20 64 61 79  2e 00                    weat day .0
+    >>>
     """
     # ruff: noqa: PLR2004
     it = iter(bytes_)
@@ -29,15 +29,18 @@ def hexdump(bytes_):
         hex_.extend(b' ' * (50 - len(hex_)))  # 3 * 16 + 2
         xd.extend(f'{i:0{d}x}: '.encode())
         xd.extend(hex_)
-        xd.extend([byte + 48 if 0 <= byte <= 9  # 48 is '0'
-            else byte + 87 if 10 <= byte <= 15  # 87 is 'a'
-            else byte if 32 <= byte <= 126      # 32-126 are the ascii printable
-            else 46 for byte in line[:8]])      # 46 is '.'
-        xd.extend(b' ')
-        xd.extend([byte + 48 if 0 <= byte <= 9  # 48 is '0'
-            else byte + 87 if 10 <= byte <= 15  # 87 is 'a'
-            else byte if 32 <= byte <= 126      # 32-126 are the ascii printable
-            else 46 for byte in line[8:]])      # 46 is '.'
+        xd.extend([32 if byte in (9, 10, 11, 13) # 32 is ' ', other are blancs
+            else byte + 48 if 0 <= byte <= 9     # 48 is '0'
+            else byte + 87 if 10 <= byte <= 15   # 87 is 'a'
+            else byte if 32 <= byte <= 126       # 32-126 are the ascii printable
+            else 46 for byte in line[:8]])       # 46 is '.'
+        if len(line) > 8:
+            xd.extend(b' ')
+        xd.extend([32 if byte in (9, 10, 11, 13) # 32 is ' ', other are blancs
+            else byte + 48 if 0 <= byte <= 9     # 48 is '0'
+            else byte + 87 if 10 <= byte <= 15   # 87 is 'a'
+            else byte if 32 <= byte <= 126       # 32-126 are the ascii printable
+            else 46 for byte in line[8:]])       # 46 is '.'
         xd.extend(b'\n')
         i += 16
     if bytes_:
