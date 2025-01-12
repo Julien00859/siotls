ac9361bb670cecae3ce01469a8c96484e9205466
Julien Castiaux <julien.castiaux@gmail.com>
2025-01-13 00:22:58 +0100

    wip

--- src/siotls/services/filestore.py
+++ src/siotls/services/filestore.py
@@ -1,9 +1,15 @@
 import collections
 import dbm
 import pathlib
+import struct
 import tempfile
+from datetime import UTC, datetime
 
 from blake3 import blake3  # 10x faster than sha512/256, for the same security
+
+struct_formats = {
+    0: f"<B{blake3.digest_size}sf"
+}
 
 
 class FileStore(collections.abc.MutableMapping):
@@ -12,31 +18,54 @@ class FileStore(collections.abc.MutableMapping):
     def __init__(self, root_path=None):
         if root_path is not None:
             self.root_path = root_path
-
         if not self.root_path.isdir():
             self.root_path.mkdir(0o775)
-        self._store = dbm.open(self.root_path / 'store.dbm', more=0o664)
+        self._store = dbm.open(self.root_path / 'store.dbm', mode=0o664)  # noqa: SIM115
+
+    def _parse(self, key, entry):
+        version = int.from_bytes(entry[0], 'little')
+        try:
+            return struct_formats[version].unpack(entry)
+        except (KeyError, ValueError) as exc:
+            raise KeyError(key) from exc
 
     def __getitem__(self, key):
-        checksum, expire = self._store[key]
-        try:
-            data = (self.root_path / checksum).read_bytes()
-        except OSError as exc:
-            e = "unable to access or read the file from the store for key"
-            raise KeyError(e) from exc
-        return data, expire
+        match self._parse(key, self._store[key]):
+            case (0, checksum, expire_timestamp):
+                filepath = self.root_path / checksum.hex()
+                try:
+                    data = filepath.read_bytes()
+                except OSError as exc:
+                    exc_ = KeyError(key)
+                    exc_.add_note(f"unable to access or read the file at {filepath}")
+                    raise exc_ from exc
+
+                return data, datetime.fromtimestamp(expire_timestamp, UTC)
 
     def __setitem__(self, key, item):
         data, expire = item
-        checksum = blake3(data).hexdigest()
-        filepath = self.root_path / checksum
 
+        # using the checkum as filename has two advantages: (1) it makes
+        # sure there are no weird chacaters, (2) it makes so a same file
+        # with different keys is only stored once.
+        checksum = blake3(data).digest()
+        filepath = self.root_path / checksum.hex()
         if not filepath.isfile():
             with filepath.open('wb') as file:
                 file.chmod(0o664)
                 file.write(data)
 
-        self._store[key] = (checksum, expire)
+        version = 0
+        self._store[key] = struct.pack(
+            struct_formats[version],
+            (version, checksum, expire.timestamp())
+        )
+
+    def __delitem__(self, key):
+        entry = self._store.pop(key)
+        match self._parse(key, entry):
+            case (0, checksum, _):
+                (self.root_path / checksum.hex()).unlink()
 
     def close(self):
         self._store.close()
--- src/siotls/services/providers/simple/simple.py
+++ src/siotls/services/providers/simple/simple.py
@@ -1,8 +1,14 @@
 import logging
+import random
 
-from siotls.services import TLSService, TLSServiceError
+from siotls.services import TLSService, TLSServiceError, TLSServiceErrorGroup
 from siotls.utils import intbyte
-from siotls.x509 import load_der_ocsp_basic_response, load_der_ocsp_response, oid
+from siotls.x509 import (
+    load_der_certificate,
+    load_der_ocsp_basic_response,
+    load_der_ocsp_response,
+    oid,
+)
 
 from ._mixins import CacheMixin, RequestMixin
 
@@ -19,7 +25,7 @@ logger = logging.getLogger(__package__)
 
 class OCSPService(TLSService, CacheMixin, RequestMixin):
     request_content_type = b'application/ocsp-request'
-    request_body_max_length = intbyte(512)
+    request_body_max_length = intbyte(1024)
 
     response_content_type = b'application/ocsp-response'
     response_body_max_length = intbyte('32kiB')  # longest ocsp res I have is 12kiB
@@ -49,14 +55,48 @@ class OCSPService(TLSService, CacheMixin, RequestMixin):
 
         return ocsp_res
 
-class CRTService(TLSService, RequestMixin):
+class CRTService(TLSService, RequestMixin, CacheMixin):
     request_content_type = ''
     request_max_length = 0
 
     response_content_type = b'application/pkix-cert'
     response_max_length = intbyte('64kiB')  # longest cert chain I have is 16kiB
 
-    def request(self, url):
+    def request(self, urls):
+        if not urls:
+            e = "missing url"
+            raise ValueError(e)
+
+        if len(urls) == 1:
+            return urls[0], self._request_single(urls[0])
+
+        for url in urls:
+            if cert := self._cache_get(url):
+                return url, cert
+
+        # Trying each URL sequentially isn't very effective... Ideally
+        # we should do as many concurrent happy-eyeballs (RFC8305) as
+        # there are URLs. So we can download from the single best one.
+        random.shuffle(urls)  # don't always reach for the first one
+        excs = []
+        for url in urls:
+            try:
+                return self._process(self._request(url))
+            except TLSServiceError as exc:
+                excs.append(exc)
+                continue
+        e = "all URLs failed"
+        raise TLSServiceErrorGroup(e, excs)
+
+    def _process(self, cert_res):
+        try:
+            cert = load_der_certificate(cert_res)
+        except ValueError as exc:
+            e = "error while loading certificate"
+            raise TLSServiceError(e) from exc
+
+        return cert_res, cert
+
 
 
 class CRLService(TLSService, RequestMixin):
@@ -64,5 +104,5 @@ class CRLService(TLSService, RequestMixin):
     request_max_length = 0
 
     response_content_type = b'application/pkix-crl'
-    response_max_length = intbyte('16MiB')< 1 << 24  # 16MiB, longest crl I have (DigitCert) is 7MiB
+    response_max_length = intbyte('16MiB')  # longest crl I have (DigitCert) is 7MiB
 
