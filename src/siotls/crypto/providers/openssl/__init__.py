7b2a69cd2205f4e1dd60fca058c06d464c35aad5
Julien Castiaux <julien.castiaux@gmail.com>
2025-02-08 01:36:27 +0100

    wip

--- src/siotls/crypto/providers/openssl/__init__.py
+++ src/siotls/crypto/providers/openssl/__init__.py
@@ -1,5 +1,2 @@
+from . import cipher_suites, key_exchanges, signature_schemes
 from .trust_store import get_trust_store
-
-
-def install():
-    from . import cipher_suites, key_exchanges, signature_schemes
